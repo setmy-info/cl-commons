1602250acb205bf604b88c3e53ecd7ba9f1f6839
Imre Tabur <imre.tabur@mail.ee>
2023-11-23 12:18:12 +0200

    Text ending check

--- src/string/operations.lisp
+++ src/string/operations.lisp
@@ -7,6 +7,7 @@
           :concat 
           :+comma-string+
           :+empty-string+
+          :ends-with-p
   )
 )
 (in-package :cl-commons/string/operations)
@@ -17,3 +18,17 @@
 (defun concat (&rest strings)
   "Concatenate and return an arbitrary number of strings as one string."
   (apply #'concatenate 'string strings))
+
+(defun ends-with-p (text-string ending)
+  "Checks if TEXT-STRING ends with the specified ENDING string, ignoring case."
+  (let*
+    (
+      (text-string-length (length text-string))
+      (ending-length (length ending))
+    )
+    (and 
+      (>= text-string-length ending-length)
+      (string-equal (subseq text-string (- text-string-length ending-length)) ending)
+    )
+  )
+)
--- tests/string/operations.lisp
+++ tests/string/operations.lisp
@@ -1,7 +1,10 @@
 (in-package :cl-user)
 (defpackage cl-commons/tests/string/operations
   (:use :cl :rove)
-  (:import-from :cl-commons/string/operations :concat))
+  (:import-from :cl-commons/string/operations 
+    :concat
+    :ends-with-p
+  ))
 (in-package :cl-commons/tests/string/operations)
 
 (deftest test-concat
@@ -9,5 +12,20 @@
     (ok (equal (concat "A" "b") "Ab"))
     (ok (equal (concat "A") "A"))
     (ok (equal (concat nil "A" nil "b") "Ab"))
+  )
+)
+
+(deftest test-ends-with-p
+  (testing "ends-with-p"
+    (ok (ends-with-p "World" "World"))
+    (ok (ends-with-p "World" "wOrld"))
+    (ok (ends-with-p "Hello, World" "World"))
+    (ok (ends-with-p "Hello, World" "wOrld"))
+    (ok (ends-with-p "Tere, Maailm" "maailm"))
+    (ok (not (ends-with-p "Programming" "gram")))
+    (ok (not (ends-with-p "Test" "AbcTest")))
+    (ok (not (ends-with-p "Test" "TestAbc")))
+    (ok (ends-with-p "12345" "5"))
+    (ok (ends-with-p "" ""))
   )
 )
